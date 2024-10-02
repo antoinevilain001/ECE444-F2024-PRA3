49b99738341fcd6e62dfa3053af841b79a3c529e
Antoine Vilain <antoine.vilain@mail.utoronto.ca>
2024-10-02 11:59:17 -0400

    added templates index.html

--- flaskr-tdd/project/app.py
+++ flaskr-tdd/project/app.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-from flask import Flask, g
+from flask import Flask, g, render_template
 
 
 # configuration
@@ -44,9 +44,13 @@ def close_db(error):
         g.sqlite_db.close()
 
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/')
+def index():
+    """Searches the database for entries, then displays them."""
+    db = get_db()
+    cur = db.execute('select * from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('index.html', entries=entries)
 
 
 if __name__ == "__main__":
